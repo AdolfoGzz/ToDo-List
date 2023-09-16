d5984288f1d9abfce7cbd707f5c291785bb7dda3
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2023-09-15 20:04:27 -0600

    fixed bug with reloading the page

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 const inputBox = document.querySelector(".add-items-input");
 const listContainer = document.querySelector(".list-container");
 
-function saveTask(){
-    localStorage.setItem("listContainer",listContainer.innerHTML);
+function saveTask() {
+    localStorage.setItem("listContainer", listContainer.innerHTML);
 }
 
-function loadTask(){
-    if(localStorage.getItem("listContainer")){
-        listContainer.innerHTML=localStorage.getItem("listContainer");
+function loadTask() {
+    if (localStorage.getItem("listContainer")) {
+        listContainer.innerHTML = localStorage.getItem("listContainer");
+
+        // Attach event listeners to the loaded tasks
+        const tasks = listContainer.querySelectorAll("li");
+        tasks.forEach((task) => {
+            task.addEventListener("click", function () {
+                task.classList.toggle("checked");
+                saveTask();
+            });
+
+            const span = document.createElement("span");
+            span.innerText = "\u00D7";
+            span.addEventListener("click", function () {
+                task.remove();
+                saveTask();
+            });
+
+            task.appendChild(span);
+        });
     }
 }
 
@@ -33,6 +51,17 @@ function addTask(){
             saveTask();
         });
         saveTask();
+    }
+}
+
+function checkInputLength() {
+    const inputText = inputBox.value;
+    const messageElement = document.querySelector(".input-message");
+
+    if (inputText.length > 35) {
+        messageElement.textContent = "Input exceeds 35 characters.";
+    } else {
+        messageElement.textContent = "";
     }
 }
 
