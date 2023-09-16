e453420bce2cf774624f9cad6135fbd848d26b1c
Adolfo Gonzalez <118020837+AdolfoGzz@users.noreply.github.com>
2023-09-15 19:46:13 -0600

    All done except for bug in the local storage

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const inputBox = document.querySelector(".add-items-input");
 const listContainer = document.querySelector(".list-container");
 
+function saveTask(){
+    localStorage.setItem("listContainer",listContainer.innerHTML);
+}
+
+function loadTask(){
+    if(localStorage.getItem("listContainer")){
+        listContainer.innerHTML=localStorage.getItem("listContainer");
+    }
+}
+
 function addTask(){
     if (inputBox.value === '') {
         return;
     }else{
         let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
+        li.innerText=inputBox.value;
         listContainer.appendChild(li);
         inputBox.value="";
+        let span=document.createElement("span");
+        span.innerText="\u00D7";
+        li.appendChild(span);
+
+        li.addEventListener("click",function(){
+            li.classList.toggle("checked");
+            saveTask();
+        });
+
+        span.addEventListener("click",function(){
+            li.remove();
+            saveTask();
+        });
+        saveTask();
     }
 }
+
+
+loadTask(); 
